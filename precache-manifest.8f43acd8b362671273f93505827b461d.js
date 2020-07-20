dc0b90f957609587f87a49e9821ddde25d2b8df6
franklin vincent <franklin.vincent@gmail.com>
2020-07-20 12:39:19 +1000

    Updates

---
+++ precache-manifest.8f43acd8b362671273f93505827b461d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b84684cc06b30cb32e2fea40283501e",
+    "url": "/franklinv/search-photos-app/index.html"
+  },
+  {
+    "revision": "daa18144c4921e65db41",
+    "url": "/franklinv/search-photos-app/static/css/2.6664b3cd.chunk.css"
+  },
+  {
+    "revision": "a775bbe2f5b77592c544",
+    "url": "/franklinv/search-photos-app/static/css/main.d0544c0f.chunk.css"
+  },
+  {
+    "revision": "daa18144c4921e65db41",
+    "url": "/franklinv/search-photos-app/static/js/2.adfe6ff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/franklinv/search-photos-app/static/js/2.adfe6ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a775bbe2f5b77592c544",
+    "url": "/franklinv/search-photos-app/static/js/main.baf674c2.chunk.js"
+  },
+  {
+    "revision": "7fdead5d462583be3f1c",
+    "url": "/franklinv/search-photos-app/static/js/runtime-main.c08d1b6e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/franklinv/search-photos-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/franklinv/search-photos-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/franklinv/search-photos-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/franklinv/search-photos-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/franklinv/search-photos-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
